badf039be29a1c1b2e9b0325f786bc4d2eb195da
Giovanni Biagini <dbiagini@tulane.edu>
2020-08-26 12:39:07 -0500

    Fixed and validated one_hot_aa.py. Seems to be working as appropriate.

--- aa-matching/aa_matching_msf.py
+++ aa-matching/aa_matching_msf.py
@@ -3,6 +3,8 @@
 # aa_matching.py - Module for amino acid matching functions
 
 import re
+import os.path
+from os import path
 import Bio
 from Bio import SeqIO
 from Bio import AlignIO
@@ -11,31 +13,30 @@ from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
 import pandas as pd
 import random
+import requests
 
 # mature protein sequence offsets differ by locus
 # get offsets by examining mature protein length in IMGT/HLA alignment tool
 # https://www.ebi.ac.uk/cgi-bin/ipd/imgt/hla/align.cgi - Mature protein
 hlaProteinOffset = {
     "A" : 24, # 365 versus 341 mature
-    "B" : 24,
-    "C" : 24,
+    "B" : 22, # offset reduced by 2 to match indexes from nn-serology (RSNNS) .pat files
+    "C" : 22, # offset reduced by 2
     "DRA" : 25,
-    "DRB1" : 29,
+    "DRB1" : 27, # offset reduced by 2
     "DRB3" : 29,
     "DRB4" : 29,
     "DRB5" : 29,
     "DQA1" : 23,
-    "DQB1" : 32,
+    "DQB1" : 27, # offset reduced by 5
     "DPA1" : 31,
-    "DPB1" : 29,
+    "DPB1" : 27, # offset reduced by 2
 }
 
 def getMatureProteinOffset(locus):
         return hlaProteinOffset.get(locus, "Invalid HLA Locus")
-
-
-def adjust_offset(loc, ard_start_pos, ard_start_pos_incomplete, ard_end_pos, ard_end_pos_incomplete, prev=0,
-                  prev_inc=0):
+    
+def adjust_offset(loc, ard_start_pos, ard_start_pos_incomplete, ard_end_pos, ard_end_pos_incomplete, prev=0, prev_inc=0):
     start = ard_start_pos
     end = ard_end_pos
     start_inc = ard_start_pos_incomplete
@@ -46,9 +47,9 @@ def adjust_offset(loc, ard_start_pos, ard_start_pos_incomplete, ard_end_pos, ard
     check = count - prev
     check_inc = count_inc - prev_inc
 
-    if (check == 0):
-        new_end = end + (count - prev)
-        new_end_inc = end_inc + (count_inc - prev_inc)
+    if (check == 0): 
+        new_end = end + (count-prev)
+        new_end_inc = end_inc + (count_inc-prev_inc)
         newlist = [new_end, new_end_inc]
         return newlist
     else:
@@ -61,7 +62,6 @@ def adjust_offset(loc, ard_start_pos, ard_start_pos_incomplete, ard_end_pos, ard
         else:
             new_end_inc = end_inc
             return adjust_offset(loc, start, start_inc, new_end, new_end_inc, prev, prev_inc=0)
-
 
 def remove_ins(loc_full_alseq):
     # need to remove the inserts from the reference sequence to print into the IMGT/HLA .txt file
@@ -82,9 +82,8 @@ def remove_ins(loc_full_alseq):
         jrow = jrow.replace('\n', '')
         jrow = jrow.replace(' ', '')
         loc_full_alseq[j] = jrow
-
+    
     return loc_full_alseq
-
 
 def generate_IMGT(HLA_full_alseq, dbversion):
     outfile = open("./IMGT_HLA_Full_Protein_" + str(dbversion) + ".txt", "w+")
@@ -93,6 +92,24 @@ def generate_IMGT(HLA_full_alseq, dbversion):
         outfile.write("HLA-" + allele_loctype + "\t" + str(HLA_full_alseq[allele_loctype]) + "\n")
     outfile.close()
     return
+
+'''
+def check_ver():
+    url = "https://raw.githubusercontent.com/ANHIG/IMGTHLA/Latest/version_report.txt"
+    r = requests.get(url)
+    regex = "\d\.\d\d\.\d"
+    result = re.search(regex, r.text)
+    curfile = "./msf/msfversion.txt"
+    with (curfile, "r+") as handle:
+        line = handle.readline().rstrip()
+        if line == result.group(0):
+            same = "yes"
+        else:
+            handle.seek(0)
+            handle.write(result.group(0))
+            same = "no"
+    return [same, result.group(0)]
+'''
 
 
 # use only ARD positions
@@ -163,7 +180,7 @@ refseq = {
     "B" : "B*07:02:01:01",
     "C" : "C*01:02:01:01",
     "DRB1" : "DRB1*01:01:01:01",
-    "DRB3" : "DRB3*01:01:01:01",
+    "DRB3" : "DRB3*01:01:02:01",
     "DRB4" : "DRB4*01:01:01:01",
     "DRB5" : "DRB5*01:01:01:01",
     "DQA1" : "DQA1*01:01:01:01",
@@ -173,20 +190,33 @@ refseq = {
     }
 
 HLA_full_allele = {} # Full four-field allele names
+HLA_full_alseq = {}
 HLA_seq = {} # Two-field
 regex = '\w*\*\d*\:\d*'
 
-## TODO: automate retrieval of newer versions of HLA/IMGT DB if not present on the system
 quest = input("Specify HLA/IMGT DB version? (y/n) ")
 if (quest == "y") or (quest == "Y"):
     dbversion = input("HLA/IMGT DB version? (x.xx.x) ")
     dbversion = dbversion.strip()
     dbversion = dbversion.replace('.','')
 else:
-    dbversion = "3390"
+    dbversion = "3400"
 
 for locus in loci:
+    loc_full_alseq = {}
     seq_filename = "./msf/" + locus + "_prot_" + str(dbversion) + ".msf"
+    #old_filename = seq_filename
+    #new_filename = "./msf/" + locus + "_prot_" + str(dbversion) + ".msf"
+    #os.rename(r(old_filename),r(new_filename))
+    if path.exists(seq_filename) == False:
+        print("Downloading requested MSF files for locus " + locus + "...")
+        url = "https://raw.githubusercontent.com/ANHIG/IMGTHLA/" + str(dbversion) + "/msf/" + locus + "_prot.msf"
+        r = requests.get(url)
+        #seq_filename = "./msf/" + locus + "_prot_" + str(dbversion) + ".msf"
+        with open(seq_filename, 'wb') as f:
+            f.write(r.content)
+    else: 
+        print("MSF files already downloaded")
     multipleseq = AlignIO.read(seq_filename, format="msf")
 
     for record in multipleseq:
@@ -195,6 +225,9 @@ for locus in loci:
         loc_full_allele = record.id
         loc_two_field_allele = re.match(regex, loc_full_allele).group()
         full_protein = record.seq
+        nogap = full_protein
+
+        loc_full_alseq[loc_full_allele] = SeqRecord(nogap)
 
         # skip missing sequences in IMGT/HLA file
         if (len(full_protein) <10): # #NAME?
@@ -204,20 +237,11 @@ for locus in loci:
         (loc,full_allele) = loc_full_allele.split("*")
         mature_protein = full_protein[getMatureProteinOffset(loc):]
 
-        ## TODO: There could still be an issue if there are '-' at the end of the ARD (or, worse, past the initial but before the final). Test cases?
         
         if loc_full_allele == refseq[loc]:
-            start = ard_start_pos[loc]
-            end = ard_end_pos[loc]
-            start_inc = ard_start_pos_incomplete[loc]
-            end_inc = ard_end_pos_incomplete[loc]
-            count = mature_protein[start:end].count('-')
-            count_inc = mature_protein[start_inc:end_inc].count('-')
-
-            ard_end_pos[loc] = end + count
-            ard_end_pos_incomplete[loc] = end + count 
-
-
+            new_end, new_end_inc = adjust_offset(loc, ard_start_pos[loc], ard_start_pos_incomplete[loc], ard_end_pos[loc], ard_end_pos_incomplete[loc], prev=0, prev_inc=0)
+            ard_end_pos[loc] = new_end
+            ard_end_pos_incomplete[loc] = new_end_inc
 
         #print(mature_protein)
 
@@ -239,16 +263,23 @@ for locus in loci:
         # https://www.biostars.org/p/57549/
 
         # print (HLA_seqrecord_dict[allele].seq)
+    
+    #!GB!# Commented out these two lines as well as the generate_IMGT() function call, since the process is intensive and does not need to be
+    #!GB!# run every time the code is used. Might should consider a switch to splitting a HLA_full_alseq DataFrame by locus and doing all of the work in 
+    #!GB!# a single function definition.
+    #loc_full_alseq = remove_ins(loc_full_alseq)
+    #HLA_full_alseq.update(loc_full_alseq)
 
+#generate_IMGT(HLA_full_alseq, dbversion)
 
 # get the AA mature protein subsequence of any HLA allele
 # Python strings start at zero, but amino acid sequences start at position 1
 def getAAsubstring(allele,start_position,end_position):
-	return HLA_seq[allele].seq[start_position+1:end_position]
+	return HLA_full_allele[allele].seq[start_position+1:end_position]
 
 # get the AA at any position of any HLA allele
 def getAAposition(allele,position):
-	return HLA_seq[allele].seq[position+1]
+	return HLA_full_allele[allele].seq[position+1]
 
 # get the SFVT epitope name (with underscore)
 def getEpitope(allele,position_list):
--- aa-matching/aa_poly_to_alleles_msf.py
+++ aa-matching/aa_poly_to_alleles_msf.py
@@ -1,21 +1,56 @@
+#TODO - OOP approach to aa-matching and sequence information
+
 import pandas as pd
 import aa_matching_msf as aa_mm
+import faulthandler
 from collections import OrderedDict
+
+def ungap(dataframe, refseq, loc):
+    # the dashes will be put at the beginning of every set of possible polymorphisms per residue
+    ## this is to prevent all of the '-' characters from being sent to front
+    #output_frame = output_frame.sort_index(axis=1, key = lambda x: (int(x[1])))
+    i = 0
+    j = 0
+    new_cols = {}
+
+    for column in dataframe:
+        num = int(column[1:])
+        if (column[0] == '-'):
+            if dataframe.loc[refseq[loc]][column] == 1:
+                i += 1
+                j += 1
+                new_col = (str(num - i) + '_INS_' + str(j))
+                new_cols[column] = new_col
+            else: 
+                j = 0
+                new_col = (str(num - i) + '_DEL_' + str(j))
+                new_cols[column] = new_col
+        else:
+            if j == 0:
+                new_col = (str(column[0]) + str(num - i))
+                new_cols[column] = new_col
+            else:
+                new_col = (str(column[0]) + str(num - i) + '_INS_' + str(j))
+                new_cols[column] = new_col
+
+    
+    dataframe = dataframe.rename(columns=new_cols)
+    return dataframe
 
 # generate list of IMGT/HLA alleles that have each single AA polymorphism
 
 refseq = {
-    "A" : "A*01:01",
-    "B" : "B*07:02",
-    "C" : "C*01:02",
-    "DRB1" : "DRB1*01:01",
-    "DRB3" : "DRB3*01:01",
-    "DRB4" : "DRB4*01:01",
-    "DRB5" : "DRB5*01:01",
-    "DQA1" : "DQA1*01:01",
-    "DQB1" : "DQB1*05:01",
-    "DPA1" : "DPA1*01:03",
-    "DPB1" : "DPB1*01:01",
+    "A" : "A*01:01:01:01",
+    "B" : "B*07:02:01:01",
+    "C" : "C*01:02:01:01",
+    "DRB1" : "DRB1*01:01:01:01",
+    "DRB3" : "DRB3*01:01:02:01",
+    "DRB4" : "DRB4*01:01:01:01",
+    "DRB5" : "DRB5*01:01:01:01",
+    "DQA1" : "DQA1*01:01:01:01",
+    "DQB1" : "DQB1*05:01:01:01",
+    "DPA1" : "DPA1*01:03:01:01",
+    "DPB1" : "DPB1*01:01:01:01",
     }
 
 
@@ -24,7 +59,7 @@ for loc in aa_mm.ard_start_pos:
     HLA_alleles = []
     AA_polys = {}
     # print (hlaProteinOffset[hla])
-    for allele_loctyp in aa_mm.HLA_seq:
+    for allele_loctyp in aa_mm.HLA_full_allele:
         (allele_loc, allele_typ) = allele_loctyp.split('*')
         if (allele_loc != loc):
             continue
@@ -41,7 +76,7 @@ for loc in aa_mm.ard_start_pos:
             if AA_poly not in AA_polys.keys():
                 AA_polys[AA_poly] = 0
                 
-    for Xallele_loctyp in aa_mm.HLA_seq:
+    for Xallele_loctyp in aa_mm.HLA_full_allele:
         (Xallele_loc, Xallele_typ) = Xallele_loctyp.split('*')
         if (Xallele_loc != loc):
             continue
@@ -65,6 +100,7 @@ for loc in aa_mm.ard_start_pos:
                     HLA_AA_polys[poly] = 1
 
             
+        ## lambda function to sort on the number following the AA residue (i.e. the residue position), then on the actual character for the residue 
         outie = OrderedDict(sorted(HLA_AA_polys.items(), key = lambda x: ((int(x[0][1:])), str(x[0][0]))))
         outie['allele'] = Xallele_loctyp
         outie.move_to_end('allele', last=False)
@@ -76,35 +112,7 @@ for loc in aa_mm.ard_start_pos:
     output_frame = pd.DataFrame(HLA_alleles)
     output_frame = output_frame.set_index('allele')
 
-    # the dashes will be put at the beginning of every set of possible polymorphisms per residue
-    ## lambda function to sort on the number following the AA residue (i.e. the residue position) 
-    ## this is to prevent all of the '-' characters from being sent to front
-    #output_frame = output_frame.sort_index(axis=1, key = lambda x: (int(x[1])))
+    output_frame = ungap(output_frame, refseq, loc)
 
-    i = 0
-    j = 0
-    new_cols = {}
-    last = aa_mm.ard_start_pos[loc]
-
-    for column in output_frame:
-        num = int(column[1:])
-        if (column[0] == '-'):
-            if output_frame.loc[refseq[loc]][column] == 1:
-                i += 1
-                new_col = (str(num - i) + '_insert_' + str(j))
-                new_cols[column] = new_col
-                j += 1
-            else: 
-                j = 1
-                new_col = (str(num - i) + '_insert_' + str(j))
-                new_cols[column] = new_col
-        else:
-            new_col = (str(column[0]) + str(num - i))
-            new_cols[column] = new_col
-
-    
-    output_frame = output_frame.rename(columns=new_cols)
-
-
-    output_frame.to_csv('aa-matching/output/' + loc + '_AA_poly.csv', index=True)
+    output_frame.to_csv('./output/' + loc + '_AA_poly.csv', index=True)
     # print (HLA_AA_AlleleList[])
